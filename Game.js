dacdb96d738ebe0a907134c606009e89c25cc8f5
ZaynBukhari <107154714+ZaynBukhari@users.noreply.github.com>
2023-05-01 05:01:28 -0700

    Update Game.js

--- Game.js
+++ Game.js
@@ -2,6 +2,7 @@ let card1;
 let card2;
 let clickedSets = [];
 let usedSets = [];
+const tcgLegalCache = {};
 let timerInterval;
 let startTime;
 
@@ -70,8 +71,8 @@ async function generateRandomCards() {
   let card1, card2, card1Sets, card2Sets;
 
   do {
-    card1 = await getRandomCard();
-    card2 = await getRandomCard();
+    const randomCardPromises = [getRandomCard(), getRandomCard()];
+    [card1, card2] = await Promise.all(randomCardPromises);
 
     card1Sets = card1.card_sets?.map(set => set.set_name) ?? [];
     card2Sets = card2.card_sets?.map(set => set.set_name) ?? [];
@@ -82,41 +83,47 @@ async function generateRandomCards() {
   return [card1, card2];
 }
 
+
 async function getRandomCard() {
   let cardData;
-  do {
-    const response = await fetch('https://db.ygoprodeck.com/api/v7/randomcard.php');
-    cardData = await response.json();
-  } while (!await isTcgLegal(cardData.id));
+  let tcgLegalCards = [];
+
+  while (tcgLegalCards.length === 0) {
+    const randomCardPromises = Array(5).fill().map(() => fetchRandomCard());
+    const randomCards = await Promise.all(randomCardPromises);
+
+    const cardIds = randomCards.map(card => card.id).join(',');
+
+    const response = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${cardIds}&format=tcg`);
+    const cardDataList = await response.json();
+
+    tcgLegalCards = cardDataList.data.filter(card => {
+      const cardInfo = randomCards.find(randomCard => randomCard.id === card.id);
+      return cardInfo && !isSingleCardSet(card);
+    });
+  }
+
+  cardData = tcgLegalCards[Math.floor(Math.random() * tcgLegalCards.length)];
+
   return cardData;
 }
 
-
-async function isTcgLegal(cardId) {
-  const response = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${cardId}&format=tcg`);
+async function fetchRandomCard() {
+  const response = await fetch('https://db.ygoprodeck.com/api/v7/randomcard.php');
   const cardData = await response.json();
-
-  if (cardData.error) {
-    return false;
-  }
-
-  const setCodes = new Set();
-if (Array.isArray(cardData.card_sets)) {
-  for (const set of cardData.card_sets) {
-    setCodes.add(set.set_code);
-  }
+  return cardData;
 }
-  if (setCodes.size === 1) {
-    const setCode = [...setCodes][0];
-    const response = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?set=${setCode}&type=Monster`);
-    const cardList = await response.json();
-    if (cardList.data.length === 1) {
-      return false;
+
+function isSingleCardSet(card) {
+  const setCodes = new Set();
+  if (Array.isArray(card.card_sets)) {
+    for (const set of card.card_sets) {
+      setCodes.add(set.set_code);
     }
   }
-
-  return true;
+  return setCodes.size === 1;
 }
+
 
 function hasIntersection(array1, array2) {
   return array1.some(item => array2.includes(item));
@@ -221,18 +228,17 @@ function updateTimer() {
 
 function resetGame() {
 
-  clearInterval(timerInterval);
+  
   card1 = null;
   card2 = null;
+  clickedSets = [];
+  usedSets = [];
+  clearInterval(timerInterval);
+  usedSetsList.innerHTML = '';
   document.getElementById('currentCard').innerHTML = '';
   document.getElementById('currentCardSets').innerHTML = '';
   document.getElementById('setCounter').innerHTML = '';
   document.getElementById('usedSets').innerHTML = '';
-  usedSets = [];
-  clickedSets = [];
-  usedSetsList.innerHTML = '';
-  
-
 
   const completionScreen = document.getElementById('completionScreen');
   console.log('Before adding hidden class:', completionScreen.classList);
@@ -289,56 +295,58 @@ function displaySetCards(setName) {
 
 
 
-function displayCurrentCard(card, isSecondCard = false) {
-  return new Promise((resolve) => {
-    const currentCardContainer = document.getElementById('currentCard');
-    const targetCardContainer = document.getElementById('targetCard');
-    const currentCardSets = document.getElementById('currentCardSets');
-    const currentCardName = document.getElementById('currentCardName');
-    const targetCardName = document.getElementById('targetCardName');
-    
+async function displayCurrentCard(card, isSecondCard = false) {
+  const currentCardContainer = document.getElementById('currentCard');
+  const targetCardContainer = document.getElementById('targetCard');
+  const currentCardSets = document.getElementById('currentCardSets');
+  const currentCardName = document.getElementById('currentCardName');
+  const targetCardName = document.getElementById('targetCardName');
 
-    if (isSecondCard) {
-      targetCardContainer.innerHTML = '';
-      displayCard(targetCardContainer, card);
-      targetCardName.textContent = card.name;
-      resolve();
-    } else {
-      currentCardContainer.innerHTML = '';
-      currentCardSets.innerHTML = '';
+  const response = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${encodeURIComponent(card.name)}&includeAliased&format=tcg`);
+  const allArtworks = await response.json();
 
-      const cardImage = document.createElement('img');
-      const cardName = document.createElement('h3');
-      cardImage.src = card.card_images[0].image_url;
-      cardName.textContent = card.name;
-      currentCardContainer.appendChild(cardImage);
-      currentCardContainer.appendChild(cardName);
-      currentCardName.textContent = card.name;
+  if (isSecondCard) {
+    targetCardContainer.innerHTML = '';
+    displayCard(targetCardContainer, card);
+    targetCardName.textContent = card.name;
+  } else {
+    currentCardContainer.innerHTML = '';
+    currentCardSets.innerHTML = '';
 
+    const cardImage = document.createElement('img');
+    const cardName = document.createElement('h3');
+    cardImage.src = card.card_images[0].image_url;
+    cardName.textContent = card.name;
+    currentCardContainer.appendChild(cardImage);
+    currentCardContainer.appendChild(cardName);
+    currentCardName.textContent = card.name;
 
-      if (card.card_sets) {
-        const seenSetNames = new Set();
-        card.card_sets.forEach(set => {
-          if (!seenSetNames.has(set.set_name)) {
-            seenSetNames.add(set.set_name);
-            const listItem = document.createElement('li');
-            listItem.textContent = `${set.set_name} (${set.set_code})`;
-            listItem.addEventListener('click', () => {
-              displaySetCards(set.set_name);
-            });
-            currentCardSets.appendChild(listItem);
-          }
+    const setNames = new Set();
+    allArtworks.data.forEach(artwork => {
+      if (artwork.card_sets) {
+        artwork.card_sets.forEach(set => {
+          setNames.add(set.set_name);
         });
       }
+    });
+    
+    const sortedSetNames = Array.from(setNames).sort();
 
+    sortedSetNames.forEach(setName => {
+      const artwork = allArtworks.data.find(artwork => artwork.card_sets.some(set => set.set_name === setName));
+      const setCode = artwork.card_sets.find(set => set.set_name === setName).set_code;
+      const listItem = document.createElement('li');
+      listItem.textContent = `${setName} (${setCode})`;
+      listItem.addEventListener('click', () => {
+        displaySetCards(setName);
+      });
+      currentCardSets.appendChild(listItem);
+    });
 
-      window.scrollTo(0, 0);
-      
-
-      resolve();
-    }
-  });
+    window.scrollTo(0, 0);
+  }
 }
+
 
 function displayCard2Sets(card) {
   const targetCardSets = document.getElementById('targetCardSets');
